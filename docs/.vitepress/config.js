cff7ca50620d9a22acfb1fe5c5ceb7b5a28290d2
Saveliy <62023376+SaveliyShutov@users.noreply.github.com>
2022-11-29 21:16:50 +0400

    add sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,40 +1,19 @@
+const privacySidebar = { text: 'Section Title A', items: [{ text: 'Item A', link: '/item-a' }, { text: 'Item B', link: '/item-b' },] }
+
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-    title: 'Хочу в Поездку',
-    description: 'Крутой инструмент',
-    lang: 'ru-RU',
-    themeConfig: {
-        nav: [
-            { text: 'Privacy', link: '/privacy/main.html' },
-            { text: 'Главная', link: '/' },
-            { text: 'Privacy', link: '/privacy/main.html' },
-          ],
-        sidebar: [
-            {
-                text: 'Section Title A',
-                items: [
-                  { text: 'Item A', link: '/item-a' },
-                  { text: 'Item B', link: '/item-b' },
-                  
-                ]
-              },
-              {
-                text: 'Section Title A',
-                items: [
-                  { text: 'Item A', link: '/item-a' },
-                  { text: 'Item B', link: '/item-b' },
-                  
-                ]
-              },
-              {
-                text: 'Section Title A',
-                items: [
-                  { text: 'Item A', link: '/item-a' },
-                  { text: 'Item B', link: '/item-b' },
-                  
-                ]
-              },
-            ]
+  title: 'Хочу в Поездку',
+  description: 'Крутой инструмент',
+  lang: 'ru-RU',
+  themeConfig: {
+    nav: [
+      { text: 'Privacy', link: '/privacy/main.html' },
+      { text: 'Главная', link: '/' },
+      { text: 'Privacy', link: '/privacy/main.html' },
+    ],
+    sidebar: {
+      '/privacy/main': privacySidebar,
     }
+  }
 })
