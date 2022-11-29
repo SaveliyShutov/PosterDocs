17501ce04057d32e1294aa582110e8d8b719ebde
Saveliy <62023376+SaveliyShutov@users.noreply.github.com>
2022-11-29 21:39:20 +0400

    Add normal content and dynamic sidebar :)

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,3 @@
-const privacySidebar = { text: 'Section Title A', items: [{ text: 'Item A', link: '/item-a' }, { text: 'Item B', link: '/item-b' },] }
-
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
@@ -8,12 +6,43 @@ export default defineConfig({
   lang: 'ru-RU',
   themeConfig: {
     nav: [
-      { text: 'Privacy', link: '/privacy/main.html' },
-      { text: 'Главная', link: '/' },
-      { text: 'Privacy', link: '/privacy/main.html' },
+      { text: 'Условия', link: '/privacy/main' },
+      { text: 'Обучение', link: '/education/main' },
+      { text: 'О проекте', link: '/about/main' },
     ],
     sidebar: {
-      '/privacy/main': privacySidebar,
+      '/privacy/': [
+        {
+          text: 'Условия',
+          items: [
+            // This shows `/guide/index.md` page.
+            { text: 'Оферта', link: '/guide/' }, // /guide/index.md
+            { text: 'Правила', link: '/guide/one' }, // /guide/one.md
+            { text: 'Оплата', link: '/guide/two' } // /guide/two.md
+          ]
+        }
+      ],
+      '/about/': [
+        {
+          text: 'О проекте',
+          items: [
+            // This shows `/guide/index.md` page.
+            { text: 'Вопросы', link: '/guide/' }, // /guide/index.md
+            { text: 'Поддержка', link: '/guide/one' }, // /guide/one.md
+            { text: 'Контакты', link: '/guide/two' } // /guide/two.md
+          ]
+        }
+      ],
+      '/education/': [
+        {
+          text: 'Обучение',
+          items: [
+            // This shows `/guide/index.md` page.
+            { text: 'Создать поездку', link: '/guide/' }, // /guide/index.md
+            { text: 'Регистрация', link: '/guide/one' }, // /guide/one.md
+          ]
+        }
+      ],
     }
   }
 })
